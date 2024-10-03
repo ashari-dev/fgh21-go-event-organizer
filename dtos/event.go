7fab5500e79014c0eee6f58a85e51d5ca2323009
ashari-dev <boykid923@gmail.com>
2024-10-03 14:53:30 +0700

    feat:add binding in struct form

--- dtos/event.go
+++ dtos/event.go
@@ -2,10 +2,10 @@ package dtos
 
 type FormEvent struct {
 	Image       string `form:"image"`
-	Title       string `form:"title"`
-	Date        string `form:"date"`
-	Description string `form:"description"`
-	LocationId  *int   `form:"locationId" `
+	Title       string `form:"title" binding:"required"`
+	Date        string `form:"date" binding:"required"`
+	Description string `form:"description" binding:"required"`
+	LocationId  *int   `form:"locationId" binding:"required"`
 }
 
 type EventCategory struct {
